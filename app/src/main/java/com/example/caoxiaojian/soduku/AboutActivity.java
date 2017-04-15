97c27166a6b4958411db1b7a2f49bcc7db7dff6f
slayercao <doomcao@163.com>
2017-04-15 12:57:45 +0800

    build basic menu and about activity

---
+++ app/src/main/java/com/example/caoxiaojian/soduku/AboutActivity.java
@@ -0,0 +1,17 @@
+package com.example.caoxiaojian.soduku;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_about);
+
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+}
--- app/src/main/java/com/example/caoxiaojian/soduku/MainActivity.java
+++ app/src/main/java/com/example/caoxiaojian/soduku/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.caoxiaojian.soduku;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_new).setOnClickListener(this);
+        findViewById(R.id.btn_resume).setOnClickListener(this);
+        findViewById(R.id.btn_about).setOnClickListener(this);
+        findViewById(R.id.btn_exit).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_new:
+
+                break;
+            case R.id.btn_resume:
+
+                break;
+            case R.id.btn_about:
+                Intent about_intent = new Intent(this, AboutActivity.class);
+                startActivity(about_intent);
+                break;
+            case R.id.btn_exit:
+                finish();
+                break;
+        }
     }
 }
