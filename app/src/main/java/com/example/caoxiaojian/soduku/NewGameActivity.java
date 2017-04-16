354a1dcff503586b7f995a232e4df7a84e143895
slayercao <doomcao@163.com>
2017-04-16 11:16:43 +0800

    add new activity

--- app/src/main/java/com/example/caoxiaojian/soduku/NewGameActivity.java
+++ app/src/main/java/com/example/caoxiaojian/soduku/NewGameActivity.java
@@ -1,14 +1,7 @@
 package com.example.caoxiaojian.soduku;
 
 import android.app.Activity;
-import android.content.Context;
-import android.graphics.Canvas;
-import android.graphics.Paint;
-import android.graphics.Path;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
-import android.view.View;
-import android.widget.LinearLayout;
 
 public class NewGameActivity extends Activity {
 
@@ -17,11 +10,8 @@ public class NewGameActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
-
         GraphicsView graphicsView = new GraphicsView(this);
-        graphicsView.getWidth();
-        graphicsView.getHeight();
+
         setContentView(graphicsView);
 
 
